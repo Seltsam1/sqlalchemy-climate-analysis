7d072d215d6912ebff965ba0ffce2a9dd8265090
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-02-27 18:26:49 -0800

    Update app.py
    Added additional routes

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ import datetime as dt
 
 import numpy as np
 
+
 #################################################
 # Database Setup
 #################################################
+
 database_path = "./Resources/hawaii.sqlite"
 
 engine = create_engine(f"sqlite:///{database_path}")
@@ -30,6 +32,7 @@ Station = Base.classes.measurement
 #################################################
 # Flask Setup
 #################################################
+
 app = Flask(__name__)
 
 
@@ -48,8 +51,10 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+        f"<p></p>"
+        f"Note - manually enter desired dates for 'start' and 'end' points of routes in format yyyy-mm-dd"
     )
 
 # Route for json dictionary of precipitation data for past year
@@ -75,6 +80,7 @@ def precipitation():
     # JSON dctionary of results
     return jsonify(precipitation_dict)
 
+
 # Route for json list of stations
 @app.route("/api/v1.0/stations")
 def stations():
@@ -94,6 +100,7 @@ def stations():
 
     # JSON list of results
     return jsonify(station_list)
+
 
 # Route for dates and temperature for most active station for past year
 @app.route("/api/v1.0/tobs")
@@ -118,10 +125,68 @@ def tobs():
     # JSON list of results
     return jsonify(temp_list)
 
-        
+
+# Routes for minimum, maximum, and average temperate for a given starting date and for a range of dates
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperatue(start=None, end=None):
+
+    # Start session with database
+    session = Session(engine)
+
+    # Variable to use in query
+    sel = [
+       func.min(Measurement.tobs),
+       func.max(Measurement.tobs),
+       func.avg(Measurement.tobs)
+    ]
+
+    # if statement for start date api route
+    if not end:
+
+        # Query
+        start_results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+
+        # Close session
+        session.close()
+
+        # Convert to list
+        start_list = list(np.ravel(start_results))
+
+        # Return print results of query
+        return (
+            f"For start date {start}:</br>"
+            "<p></p>"
+            f"The lowest temperature is {start_list[0]}.</br>"
+            f"The highest temperture is {start_list[1]}.</br>"
+            f"The average temperature is {round((start_list[2]),2)}.</br>"
+        )
+    # else statement for start and end date api route
+    else:
+
+        # Query
+        range_results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+
+        # Close session
+        session.close()
+
+        # Convert to list
+        range_list = list(np.ravel(range_results))
+
+        # Return print results of query
+        return (
+            f"For start date {start} and end date {end}:</br>"
+            "<p></p>"
+            f"The lowest temperature is {range_list[0]}.</br>"
+            f"The highest temperture is {range_list[1]}.</br>"
+            f"The average temperature is {round((range_list[2]),2)}.</br>"
+        )
 
 
-
+# To run applicaton
 
 if __name__ == "__main__":
     app.run(debug=True)
